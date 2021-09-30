cd145edadbf0f9cb9e90e2e11ce87e5c0b14b2a6
PraneshSrinivasan <psrinivasan@fleetstudio.com>
2021-09-30 16:17:33 +0530

    get,dif done

--- server.js
+++ server.js
@@ -1,58 +1,56 @@
 var express = require('express');
 var app = express();
 
-app.get('/api.github.com', function (req, res) {
-   var XMLHttpRequest = require('xhr2');
-   var xhr = new XMLHttpRequest();
-   const url=`https://api.github.com`;
-   xhr.open('GET', url, true);
-   xhr.onload = function () {
-      const dat = JSON.parse(this.response);
-      res.send(dat);
-   }
-   xhr.send();
-})
+//get commits
+app.get('/repos/:username/:reponame/commits/:sha', function(req, res) {
 
-app.get('/api.github.com/repos', function (req, res) {
    var XMLHttpRequest = require('xhr2');
    var xhr = new XMLHttpRequest();
-   const url=`https://api.github.com/repos`;
-   xhr.open('GET', url, true);
-   xhr.onload = function () {
-      const dta = JSON.parse(this.response);
-      res.send(dta);
-   }
-   xhr.send();
-})
+   var owner = req.params.username;
+   var repo = req.params.reponame;
+   var oid = req.params.sha;
+   const curl=`https://api.github.com/repos/${owner}/${repo}/commits/${oid}`;
 
-app.get('/api.github.com/repos/owner', function(req, res) {
-   
-   var XMLHttpRequest = require('xhr2');
-   var xhr = new XMLHttpRequest();
-   const url=`https://api.github.com/repos/arunnatarajs/toDoApp`;
-   xhr.open('GET', url, true);
+   xhr.open('GET', curl, true);
+
    xhr.onload = function () {
+
       const data = JSON.parse(this.response);
       res.send(data);
    }
    xhr.send();
+
 })
 
-app.get('/api.github.com/repos/owner/commits', function (req, res) {
+//get diff
+ app.get('/repos/:username/:reponame/commits/:sha/diff', function(req, res) {
+   
    var XMLHttpRequest = require('xhr2');
    var xhr = new XMLHttpRequest();
-   const url=`https://api.github.com/repos/arunnatarajs/toDoApp/commits`;
-   xhr.open('GET', url, true);
+   var username = req.params.username;
+   var repo = req.params.reponame;
+   var oid = req.params.sha;
+   const curl=`https://api.github.com/repos/${username}/${repo}/commits/${oid}`;
+
+   xhr.open('GET', curl, true);
+
    xhr.onload = function () {
-      const cdata = JSON.parse(this.response);
-      res.send(cdata);
+
+      const data = JSON.parse(this.response);
+      var dxhr = new XMLHttpRequest();
+      var psha = data.parents[0].sha;//parent sha
+      const durl=`https://api.github.com/repos/${username}/${repo}/compare/${oid}...${psha}`;//parent url
+      dxhr.open('GET', durl, true);
+
+      dxhr.onload = function () {
+         
+         const dat = JSON.parse(this.response);
+         res.send(dat);
+      }
+      dxhr.send();
    }
    xhr.send();
+
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(8081);
